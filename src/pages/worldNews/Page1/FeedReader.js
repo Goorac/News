169b8ff1459f1554c42f3c22b60f6b21e2d708f5
Goorac <82958803+Goorac@users.noreply.github.com>
2021-05-30 14:16:32 +0530

    Add files via upload

---
+++ src/pages/worldNews/Page1/FeedReader.js
@@ -0,0 +1,28 @@
+let API = "https://api.rss2json.com/v1/api.json?rss_url=";
+let userFeedURLs = [ 
+    "http://feeds.bbci.co.uk/news/world/rss.xml",
+    "http://feeds.bbci.co.uk/news/world/asia/rss.xml"
+];
+userFeedURLs.forEach(userUrl => {
+    $.ajax({
+        type: 'GET',
+        url: API + userUrl,
+        dataType: 'jsonp',
+        success: function (data) {
+            console.log(data);
+            data.items.forEach(item => {
+                var content = document.getElementById('content');
+
+                var newItem = "";
+                newItem += "<div class=\"goorac\" id=\"item\"><a href=\"" + item.link + "\"><p class=\"gh\" >" + item.title + "</p></a>";
+                if (item.author != "")
+                  
+                newItem += "<h4>Published Date: " + item.pubDate + "</h4>";
+
+                newItem += "<p class='dg'>" + item.description + "</p><hr></div>";
+
+                content.insertAdjacentHTML('beforeend', newItem);
+            });
+        }
+    });
+});
